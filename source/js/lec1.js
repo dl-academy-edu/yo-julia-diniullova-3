2c0da6c355612c19c7becec8189d69854198add4
Юлия <87200821+Juicyyyy@users.noreply.github.com>
2023-03-27 12:48:59 +0400

    правки 1 дз + 2 дз

--- source/js/lec1.js
+++ source/js/lec1.js
@@ -1,30 +1,30 @@
-let num = 11;
-let str = 'hello';
-let age = true;
-let name = undefined;
-let device = null;
-let big = 3456789009876543n;
-let color = {
-    red: '#FF0000',
-    blue: '#87CEEB',
-    white: '#FFFFFF'
-};
-let user = Symbol();
+// let peopleQuantity = 20;
+// let userName = 'Yulia';
+// let isCorrect = true;
+// let userAge = undefined;
+// let deviceInStore = null;
+// let bigintFromNumber = 3456789009876543n;
+// let colors = {
+//     red: '#FF0000',
+//     blue: '#87CEEB',
+//     white: '#FFFFFF'
+// };
+// let user = Symbol();
 
-console.log('Приведение к числу, строке и булевому типу\n');
-console.log('Число:');
-console.log(Number(num), String(num), Boolean(num));
-console.log('Строка:');
-console.log(Number(str), String(str), Boolean(str));
-console.log('Булевый тип:');
-console.log(Number(age), String(age), Boolean(age));
-console.log('undefined:');
-console.log(Number(name), String(name), Boolean(name));
-console.log('null:');
-console.log(Number(device), String(device), Boolean(device));
-console.log('Большое число:');
-console.log(Number(big), String(big), Boolean(big));
-console.log('Объект:');
-console.log(Number(color), String(color), Boolean(color));
-console.log('Символ:');
-console.log(String(user), Boolean(user));
+// console.log('Приведение к числу, строке и булевому типу\n');
+// console.log('Число:');
+// console.log(Number(peopleQuantity), String(peopleQuantity), Boolean(peopleQuantity));
+// console.log('Строка:');
+// console.log(Number(userName), String(userName), Boolean(userName));
+// console.log('Булевый тип:');
+// console.log(Number(isCorrect), String(isCorrect), Boolean(isCorrect));
+// console.log('undefined:');
+// console.log(Number(userAge), String(userAge), Boolean(userAge));
+// console.log('null:');
+// console.log(Number(deviceInStore), String(deviceInStore), Boolean(deviceInStore));
+// console.log('Большое число:');
+// console.log(Number(bigintFromNumber), String(bigintFromNumber), Boolean(bigintFromNumber));
+// console.log('Объект:');
+// console.log(Number(colors), String(colors), Boolean(colors));
+// console.log('Символ:');
+// console.log(String(user), Boolean(user));
