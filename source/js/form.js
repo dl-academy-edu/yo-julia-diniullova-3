69cf565037690af95069f0937b9553da1f19c20d
Юлия <87200821+Juicyyyy@users.noreply.github.com>
2023-04-09 18:44:26 +0400

    верстка модальных окон

--- source/js/form.js
+++ source/js/form.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('.signIn');
 const buttonOpen = document.querySelector('.header__signIn');
-const buttonClose = document.querySelector('.signIn__close');
-const overlay = document.querySelector('.signIn__overlay');
+const buttonClose = document.querySelector('.close');
+const overlay = document.querySelector('.overlay');
 
 buttonOpen.addEventListener("click", () => {
     form.classList.add('open');
