6b9880adf85c5bf5b700eb22bdfb32482ecfb70f
Юлия <87200821+Juicyyyy@users.noreply.github.com>
2023-04-09 19:48:35 +0400

    функционал модальных окон

--- source/js/form.js
+++ source/js/form.js
@@ -1,17 +1,95 @@
-const form = document.querySelector('.signIn');
-const buttonOpen = document.querySelector('.header__signIn');
-const buttonClose = document.querySelector('.close');
-const overlay = document.querySelector('.overlay');
+(function() {
+  const form = document.forms.signIn;
+  const buttonOpen = document.querySelector('.header__signIn');
+  const buttonClose = document.querySelector('.signIn__close');
+  const overlay = document.querySelector('.signIn__overlay');
 
-buttonOpen.addEventListener("click", () => {
+  if (!form) return;
+
+  buttonOpen.addEventListener("click", () => {
     form.classList.add('open');
-    input.focus();
-});
+  });
 
-buttonClose.addEventListener("click", () => {
-    form.classList.remove('open');
-});
+  buttonClose.addEventListener("click", () => {
+    modalClose();
+  });
 
-overlay.addEventListener("click", () => {
+  overlay.addEventListener("click", () => {
+    modalClose();
+  });
+
+  window.addEventListener('keydown', (e) => {
+    if(e.keyCode === 27) {
+      modalClose();
+    }
+  });
+
+  function modalClose () {
     form.classList.remove('open');
-});
+  };
+
+})();
+
+(function() {
+  const form = document.forms.register;
+  const buttonOpen = document.querySelector('.header__register');
+  const buttonClose = document.querySelector('.register__close');
+  const overlay = document.querySelector('.register__overlay');
+
+  if (!form) return;
+
+  buttonOpen.addEventListener("click", () => {
+    form.classList.add('open');
+  });
+
+  buttonClose.addEventListener("click", () => {
+    modalClose();
+  });
+
+  overlay.addEventListener("click", () => {
+    modalClose();
+  });
+
+  window.addEventListener('keydown', (e) => {
+    if(e.keyCode === 27) {
+      modalClose();
+    }
+  });
+
+  function modalClose () {
+    form.classList.remove('open');
+  };
+
+})();
+
+(function() {
+  const form = document.forms.send;
+  const buttonOpen = document.querySelector('.footer__btn');
+  const buttonClose = document.querySelector('.send__close');
+  const overlay = document.querySelector('.send__overlay');
+
+  if (!form) return;
+
+  buttonOpen.addEventListener("click", () => {
+    form.classList.add('open');
+  });
+
+  buttonClose.addEventListener("click", () => {
+    modalClose();
+  });
+
+  overlay.addEventListener("click", () => {
+    modalClose();
+  });
+
+  window.addEventListener('keydown', (e) => {
+    if(e.keyCode === 27) {
+      modalClose();
+    }
+  });
+
+  function modalClose () {
+    form.classList.remove('open');
+  };
+
+})();
