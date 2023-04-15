3410ea905321bfbe4898d7fd62c6b976a8818681
Юлия <87200821+Juicyyyy@users.noreply.github.com>
2023-04-15 19:34:41 +0400

    валидация форм

--- source/js/form.js
+++ source/js/form.js
@@ -1,92 +1,297 @@
 (function() {
-  const form = document.forms.signIn;
-  const buttonOpen = document.querySelector('.header__signIn');
-  const buttonClose = document.querySelector('.signIn__close');
-  const overlay = document.querySelector('.signIn__overlay');
+  const buttonOpen = document.querySelectorAll('[data-modal]');
 
-  if (!form) return;
+  if(!buttonOpen) return;
 
-  buttonOpen.addEventListener("click", () => {
-    form.classList.add('open');
-  });
+  const modals = document.querySelectorAll('.modal');
+  const buttonClose = document.querySelectorAll('[data-close]');
 
-  buttonClose.addEventListener("click", () => {
-    modalClose();
-  });
+  for (let element of buttonOpen) {
+    element.addEventListener('click', function() {
+      let modalId = element.dataset.modal;
+      let modal = document.getElementById(modalId);
 
-  overlay.addEventListener("click", () => {
-    modalClose();
-  });
+      modalOpen(modal);
+    })
+  }
+
+  function modalOpen(modal) {
+    modal.classList.add('open');
+  }
+
+  for (let element of buttonClose) {
+    element.addEventListener('click', function() {
+      let modalId = element.dataset.close;
+      let modal = document.getElementById(modalId);
+
+      modalClose(modal);
+    })
+  }
 
   window.addEventListener('keydown', (e) => {
-    if(e.keyCode === 27) modalClose();
+    if(e.keyCode === 27) {
+      for (let modal of modals) {
+        modal.classList.remove('open');
+      }
+    }
   });
 
-  function modalClose () {
-    form.classList.remove('open');
-  };
+  function modalClose(modal) {
+    modal.classList.remove('open');
+  }
+})();
 
+(function() {
+  const form = document.forms.signIn;
+
+  if(!form) return;
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const errorMessages = document.querySelectorAll('.invalid-feedback');
+    for (let error of errorMessages) {
+      error.remove();
+    }
+
+    const userEmail = form.elements.email;
+    const userPassword = form.elements.password;
+
+    let errors = {};
+
+    if (!isEmailCorrect(userEmail.value)) errors.email = 'Please enter a valid email address (your entry is not in the format "somebody@example.com")';
+    if (userEmail.value.length === 0) errors.email = 'This field is required';
+
+    if (userPassword.value.length < 2) errors.password = 'Too short password';
+    if (userPassword.value.length === 0) errors.password = 'This field is required';
+
+    if (Object.keys(errors).length) {
+      Object.keys(errors).forEach(key => {
+        const messageError = errors[key];
+        const input = form.elements[key];
+        setError(input, messageError);
+      })
+    }
+  })
 })();
 
 (function() {
   const form = document.forms.register;
-  const buttonOpen = document.querySelector('.header__register');
-  const buttonClose = document.querySelector('.register__close');
-  const overlay = document.querySelector('.register__overlay');
 
-  if (!form) return;
+  if(!form) return;
 
-  buttonOpen.addEventListener("click", () => {
-    form.classList.add('open');
-  });
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-  buttonClose.addEventListener("click", () => {
-    modalClose();
-  });
+    const errorMessages = document.querySelectorAll('.invalid-feedback');
+    for (let error of errorMessages) {
+      error.remove();
+    }
 
-  overlay.addEventListener("click", () => {
-    modalClose();
-  });
+    const userEmail = form.elements.email;
+    const userName = form.elements.name;
+    const userSurname = form.elements.surname;
+    const userPassword = form.elements.password;
+    const userRepeatPassword = form.elements.repeatPassword;
+    const userLocation = form.elements.location;
+    const userAge = form.elements.age;
 
-  window.addEventListener('keydown', (e) => {
-    if(e.keyCode === 27) modalClose();
-  });
+    let errors = {};
 
-  function modalClose () {
-    form.classList.remove('open');
-  };
+    if (!isEmailCorrect(userEmail.value)) errors.email = 'Please enter a valid email address (your entry is not in the format "somebody@example.com")';
+    if (userEmail.value.length === 0) errors.email = 'This field is required';
 
+    if (userName.value.length < 2) errors.name = 'Name too short';
+    if (userName.value.length === 0) errors.name = 'This field is required';
+
+    if (userSurname.value.length < 2) errors.surname = 'Surname too short';
+    if (userSurname.value.length === 0) errors.surname = 'This field is required';
+
+    if (userPassword.value.length < 2) errors.password = 'Too short password';
+    if (userPassword.value.length === 0) errors.password = 'This field is required';
+
+    if (userRepeatPassword.value.length !== userPassword) errors.repeatPassword = 'Password mismatch';
+    if (userRepeatPassword.value.length === 0) errors.repeatPassword = 'This field is required';
+
+    if (userLocation.value.length < 2) errors.location = 'Too short location';
+    if (userLocation.value.length === 0) errors.location = 'This field is required';
+
+    if (userAge.value.length === 0) errors.age = 'This field is required';
+
+    if (Object.keys(errors).length) {
+      Object.keys(errors).forEach(key => {
+        const messageError = errors[key];
+        const input = form.elements[key];
+        setError(input, messageError);
+      })
+    }
+  })
 })();
 
 (function() {
   const form = document.forms.send;
-  const buttonOpen = document.querySelector('.footer__btn');
-  const buttonClose = document.querySelector('.send__close');
-  const overlay = document.querySelector('.send__overlay');
 
-  if (!form) return;
+  if(!form) return;
 
-  buttonOpen.addEventListener("click", () => {
-    form.classList.add('open');
-  });
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-  buttonClose.addEventListener("click", () => {
-    modalClose();
-  });
+    const errorMessages = document.querySelectorAll('.invalid-feedback');
+    for (let error of errorMessages) {
+      error.remove();
+    }
 
-  overlay.addEventListener("click", () => {
-    modalClose();
-  });
+    const userName = form.elements.name;
+    const userMessageSubject = form.elements.message;
+    const userEmail = form.elements.email;
+    const userPhone = form.elements.phone;
 
-  window.addEventListener('keydown', (e) => {
-    if(e.keyCode === 27) modalClose();
-  });
+    let errors = {};
 
-  function modalClose () {
-    form.classList.remove('open');
-  };
+    if (userName.value.length < 2) errors.name = 'Name too short';
+    if (userName.value.length === 0) errors.name = 'This field is required';
 
+    if (userMessageSubject.value.length < 2) errors.message = 'Message subject too short';
+    if (userMessageSubject.value.length === 0) errors.message = 'This field is required';
+
+    if (!isEmailCorrect(userEmail.value)) errors.email = 'Please enter a valid email address (your entry is not in the format "somebody@example.com")';
+    if (userEmail.value.length === 0) errors.email = 'This field is required';
+
+    if (!isPhoneCorrect(userPhone.value)) errors.phone = 'Please enter a valid phone';
+    if (userPhone.value.length === 0) errors.phone = 'This field is required';
+
+    if (Object.keys(errors).length) {
+      Object.keys(errors).forEach(key => {
+        const messageError = errors[key];
+        const input = form.elements[key];
+        setError(input, messageError);
+      })
+    }
+  })
 })();
+
+function setError(input, messageError) {
+  if (!input[0]) {
+    setErrorText(input, messageError);
+  }
+}
+
+function setErrorText(input, messageError) {
+  const error = errorCreator(messageError);
+  input.classList.add('is-invalid');
+  input.classList.add('input__margin');
+  input.insertAdjacentElement('afterend', error);
+  input.addEventListener('input', () => {
+    error.remove();
+    input.classList.remove('is-invalid');
+  })
+}
+
+function errorCreator(errorMessage) {
+  let messageErrorContainer = document.createElement('div');
+  messageErrorContainer.classList.add('invalid-feedback');
+  messageErrorContainer.innerText = errorMessage;
+  return messageErrorContainer;
+}
+
+function isEmailCorrect(email) {
+  return email.match(/^[0-9a-z-\.]+\@[0-9a-z-]{2,}\.[a-z]{2,}$/i);
+}
+
+function isPhoneCorrect(phone) {
+  return phone.match(/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/);
+}
+
+
+// (function() {
+//   const form = document.forms.signIn;
+//   const buttonOpen = document.querySelector('.header__signIn');
+//   const buttonClose = form.querySelector('.signIn__close');
+//   const overlay = form.querySelector('.signIn__overlay');
+
+//   if (!form) return;
+
+//   buttonOpen.addEventListener("click", () => {
+//     form.classList.add('open');
+//   });
+
+//   buttonClose.addEventListener("click", () => {
+//     modalClose();
+//   });
+
+//   overlay.addEventListener("click", () => {
+//     modalClose();
+//   });
+
+//   window.addEventListener('keydown', (e) => {
+//     if(e.keyCode === 27) modalClose();
+//   });
+
+//   function modalClose () {
+//     form.classList.remove('open');
+//   };
+
+// })();
+
+// (function() {
+//   const form = document.forms.register;
+//   const buttonOpen = document.querySelector('.header__register');
+//   const buttonClose = form.querySelector('.register__close');
+//   const overlay = form.querySelector('.register__overlay');
+
+//   if (!form) return;
+
+//   buttonOpen.addEventListener("click", () => {
+//     form.classList.add('open');
+//   });
+
+//   buttonClose.addEventListener("click", () => {
+//     modalClose();
+//   });
+
+//   overlay.addEventListener("click", () => {
+//     modalClose();
+//   });
+
+//   window.addEventListener('keydown', (e) => {
+//     if(e.keyCode === 27) modalClose();
+//   });
+
+//   function modalClose () {
+//     form.classList.remove('open');
+//   };
+
+// })();
+
+// (function() {
+//   const form = document.forms.send;
+//   const buttonOpen = document.querySelector('.footer__btn');
+//   const buttonClose = form.querySelector('.send__close');
+//   const overlay = form.querySelector('.send__overlay');
+
+//   if (!form) return;
+
+//   buttonOpen.addEventListener("click", () => {
+//     form.classList.add('open');
+//   });
+
+//   buttonClose.addEventListener("click", () => {
+//     modalClose();
+//   });
+
+//   overlay.addEventListener("click", () => {
+//     modalClose();
+//   });
+
+//   window.addEventListener('keydown', (e) => {
+//     if(e.keyCode === 27) modalClose();
+//   });
+
+//   function modalClose () {
+//     form.classList.remove('open');
+//   };
+
+// })();
 
 (function() {
   const buttonTop = document.querySelector('.buttonToTop');
