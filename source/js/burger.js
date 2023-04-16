9351881ce3884a9da60c575452e0af8cde4b0d52
Юлия <87200821+Juicyyyy@users.noreply.github.com>
2023-04-16 19:39:33 +0400

    адаптив на главной странице + бургер меню

---
+++ source/js/burger.js
@@ -0,0 +1,13 @@
+const buttonMenu = document.querySelector('.burger__btn');
+const menu = document.querySelector('.burger');
+const menuClose = menu.querySelector('.burger__btn--close');
+
+buttonMenu.addEventListener("click", () => {
+  menu.classList.add('burger__open');
+  input.focus();
+});
+
+menuClose.addEventListener("click", () => {
+  menu.classList.remove('burger__open');
+  input.focus();
+});
