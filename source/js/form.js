5f0bd4ef16abfba7801cd4704392bfa3bc0968b6
Юлия <87200821+Juicyyyy@users.noreply.github.com>
2023-04-16 14:32:28 +0400

    валидация форм на истину + кнопки в формах

--- source/js/form.js
+++ source/js/form.js
@@ -41,6 +41,7 @@
   }
 })();
 
+
 (function() {
   const form = document.forms.signIn;
 
@@ -88,6 +89,11 @@
       error.remove();
     }
 
+    const goodMessages = document.querySelectorAll('.valid-feedback');
+    for (let good of goodMessages) {
+      good.remove();
+    }
+
     const userEmail = form.elements.email;
     const userName = form.elements.name;
     const userSurname = form.elements.surname;
@@ -97,26 +103,34 @@
     const userAge = form.elements.age;
 
     let errors = {};
+    let good = {};
 
-    if (!isEmailCorrect(userEmail.value)) errors.email = 'Please enter a valid email address (your entry is not in the format "somebody@example.com")';
-    if (userEmail.value.length === 0) errors.email = 'This field is required';
+    if (!isEmailCorrect(userEmail.value)) errors.email = 'Please enter a valid email address (your entry is not in the format "somebody@example.com")'
+    else if (userEmail.value.length === 0) errors.email = 'This field is required'
+    else good.email = 'All right';
 
     if (userName.value.length < 2) errors.name = 'Name too short';
-    if (userName.value.length === 0) errors.name = 'This field is required';
+    else if (userName.value.length === 0) errors.name = 'This field is required';
+    else good.name = 'All right';
 
     if (userSurname.value.length < 2) errors.surname = 'Surname too short';
-    if (userSurname.value.length === 0) errors.surname = 'This field is required';
+    else if (userSurname.value.length === 0) errors.surname = 'This field is required';
+    else good.surname = 'All right';
 
     if (userPassword.value.length < 2) errors.password = 'Too short password';
-    if (userPassword.value.length === 0) errors.password = 'This field is required';
+    else if (userPassword.value.length === 0) errors.password = 'This field is required';
+    else good.password = 'All right';
 
     if (userRepeatPassword.value.length !== userPassword) errors.repeatPassword = 'Password mismatch';
-    if (userRepeatPassword.value.length === 0) errors.repeatPassword = 'This field is required';
+    else if (userRepeatPassword.value.length === 0) errors.repeatPassword = 'This field is required';
+    else good.repeatPassword = 'All right';
 
     if (userLocation.value.length < 2) errors.location = 'Too short location';
-    if (userLocation.value.length === 0) errors.location = 'This field is required';
+    else if (userLocation.value.length === 0) errors.location = 'This field is required';
+    else good.location = 'All right';
 
-    if (userAge.value.length === 0) errors.age = 'This field is required';
+    if (userAge.value.length === 0) errors.age = 'This field is required'
+    else good.age = 'All right';
 
     if (Object.keys(errors).length) {
       Object.keys(errors).forEach(key => {
@@ -125,6 +139,14 @@
         setError(input, messageError);
       })
     }
+    if (Object.keys(good).length) {
+      Object.keys(good).forEach(key => {
+        const messageGood = good[key];
+        const input = form.elements[key];
+        setGood(input, messageGood);
+      })
+    }
+
   })
 })();
 
@@ -141,24 +163,34 @@
       error.remove();
     }
 
+    const goodMessages = document.querySelectorAll('.valid-feedback');
+    for (let good of goodMessages) {
+      good.remove();
+    }
+
     const userName = form.elements.name;
     const userMessageSubject = form.elements.message;
     const userEmail = form.elements.email;
     const userPhone = form.elements.phone;
 
     let errors = {};
+    let good = {};
 
     if (userName.value.length < 2) errors.name = 'Name too short';
-    if (userName.value.length === 0) errors.name = 'This field is required';
+    else if (userName.value.length === 0) errors.name = 'This field is required';
+    else good.name = 'All right';
 
     if (userMessageSubject.value.length < 2) errors.message = 'Message subject too short';
-    if (userMessageSubject.value.length === 0) errors.message = 'This field is required';
+    else if (userMessageSubject.value.length === 0) errors.message = 'This field is required';
+    else good.message = 'All right';
 
     if (!isEmailCorrect(userEmail.value)) errors.email = 'Please enter a valid email address (your entry is not in the format "somebody@example.com")';
-    if (userEmail.value.length === 0) errors.email = 'This field is required';
+    else if (userEmail.value.length === 0) errors.email = 'This field is required';
+    else good.email = 'All right';
 
     if (!isPhoneCorrect(userPhone.value)) errors.phone = 'Please enter a valid phone';
-    if (userPhone.value.length === 0) errors.phone = 'This field is required';
+    else if (userPhone.value.length === 0) errors.phone = 'This field is required';
+    else good.phone = 'All right';
 
     if (Object.keys(errors).length) {
       Object.keys(errors).forEach(key => {
@@ -167,12 +199,25 @@
         setError(input, messageError);
       })
     }
+    if (Object.keys(good).length) {
+      Object.keys(good).forEach(key => {
+        const messageGood = good[key];
+        const input = form.elements[key];
+        setGood(input, messageGood);
+      })
+    }
   })
 })();
 
 function setError(input, messageError) {
   if (!input[0]) {
     setErrorText(input, messageError);
+  }
+}
+
+function setGood(input, messageGood) {
+  if (!input[0]) {
+    setGoodText(input, messageGood);
   }
 }
 
@@ -187,11 +232,29 @@ function setErrorText(input, messageError) {
   })
 }
 
+function setGoodText(input, messageGood) {
+  const good = goodCreator(messageGood);
+  input.classList.add('is-valid');
+  input.classList.add('input__margin');
+  input.insertAdjacentElement('afterend', good);
+  input.addEventListener('input', () => {
+    good.remove();
+    input.classList.remove('is-valid');
+  })
+}
+
 function errorCreator(errorMessage) {
   let messageErrorContainer = document.createElement('div');
   messageErrorContainer.classList.add('invalid-feedback');
   messageErrorContainer.innerText = errorMessage;
   return messageErrorContainer;
+}
+
+function goodCreator(goodMessage) {
+  let messageGoodContainer = document.createElement('div');
+  messageGoodContainer.classList.add('valid-feedback');
+  messageGoodContainer.innerText = goodMessage;
+  return messageGoodContainer;
 }
 
 function isEmailCorrect(email) {
---
+++ source/js/form_button.js
@@ -0,0 +1,50 @@
+(function() {
+  const checkbox = document.querySelector('.register__checkbox');
+  const button = document.querySelector('.register__btn');
+
+  checkbox.onmouseenter = function() {
+    button.classList.add('btn--hover');
+  }
+
+  checkbox.onmouseleave = function() {
+    button.classList.remove('btn--hover');
+  }
+
+  checkbox.addEventListener("change", function () {
+    if (this.checked) {
+      button.classList.remove('btn--hover');
+      button.classList.add('btn--active');
+      button.removeAttribute('disabled');
+    }
+    else {
+      button.classList.remove('btn--active');
+      button.setAttribute('disabled');
+    }
+})
+})();
+
+(function() {
+  const checkbox = document.querySelector('.send__checkbox');
+  const button = document.querySelector('.send__btn');
+
+  checkbox.onmouseenter = function() {
+    button.classList.add('btn--hover');
+  }
+
+  checkbox.onmouseleave = function() {
+    button.classList.remove('btn--hover');
+  }
+
+  checkbox.addEventListener("change", function () {
+    if (this.checked) {
+      button.classList.remove('btn--hover');
+      button.classList.add('btn--active');
+      button.removeAttribute('disabled');
+    }
+    else {
+      button.classList.remove('btn--active');
+      button.setAttribute('disabled');
+    }
+})
+})();
+
